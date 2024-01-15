acd4e2fb71b9c447ef08acea84bf76e1198018fe
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-15 20:34:45 +1300

    Add SortBrew class and implement sorting and printing functionality

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import { SortBrew } from './classes'
+import { numbers } from './mock'
+
+const sortBrew = new SortBrew([10, 3, -5, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+sortBrew.sort()
+sortBrew.print()
---
+++ src/classes/index.ts
@@ -0,0 +1,1 @@
+export { SortBrew } from './SortBrew'
