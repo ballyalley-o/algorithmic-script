c3f4d5fbdeebbf9439956ea444fd8c750737bcf6
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-16 02:09:02 +1300

    Refactor SortBrew class to be abstract and implement abstract methods

--- src/classes/SortBrew.ts
+++ src/classes/SortBrew.ts
@@ -1,21 +1,23 @@
 import { ISortBrew } from '@src/interfaces'
 
-export class SortBrew {
-  constructor(public collection: ISortBrew) {}
+export abstract class SortBrew {
+  abstract length: number
+  abstract compare(leftIndex: number, rightIndex: number): boolean
+  abstract swap(leftIndex: number, rightIndex: number): void
 
   sort(): void {
-    const { length } = this.collection
+    const { length } = this
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1)
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1)
         }
       }
     }
   }
 
   print(): void {
-    console.log(this.collection)
+    console.log(this)
   }
 }
