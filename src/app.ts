5af50f69dafeabbf728e3ff4efe1708404f1de5c
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-15 23:33:20 +1300

    Add character sorting functionality

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { SortBrew, NumbersArray } from './classes'
+import { SortBrew, NumbersArray, CharactersArray } from './classes'
 import { numbers, charArray } from './mock'
 
 const numbersArray = new NumbersArray(numbers)
@@ -6,3 +6,9 @@ const sortBrew = new SortBrew(numbersArray)
 
 sortBrew.sort()
 sortBrew.print()
+
+const charactersArray = new CharactersArray('dcdd12dqwq')
+const sortBrewChar = new SortBrew(charactersArray)
+sortBrewChar.sort()
+
+console.log(charactersArray.data)
