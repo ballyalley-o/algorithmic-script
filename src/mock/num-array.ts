bfbe20cce332668988f49cc47aaca8a551ec2eaa
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-15 23:33:38 +1300

    Refactor num-array.ts

--- src/mock/num-array.ts
+++ src/mock/num-array.ts
@@ -1,9 +1,7 @@
-// Mock array of numbers
-// genrate random mixed numbers
 const numbers: number[] = Array.from({ length: 10 }, () =>
   Math.floor(Math.random() * 1100)
 )
-// Mock array of numbers using Array constructor
+
 const moreNumbers: number[] = new Array(10).fill(0).map((_, index) => index + 1)
 
 export { numbers, moreNumbers }
